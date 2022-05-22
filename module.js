ab925dc31f63e69c1b24302cc31c8c694028d2c9
pawelkudlak <coodypl@gmail.com>
2022-05-22 22:24:11 +0200

    Add animation, borders and msgs about actions

--- module.js
+++ module.js
@@ -1,42 +1,70 @@
 class Kubek {
     kolor;
     pojemnosc;
-    plyn=0;
+    czystoscKubka;
+    plyn = 0;
 
     constructor(y) {
-        this.pojemnosc=y;
+        this.pojemnosc = y;
+        this.czystoscKubka=this.kolor;
     }
 
     napelnij() {
-        this.plyn=this.pojemnosc;
-        document.getElementById("coffieCup").style.height='100px';
-        console.log('Kubek zostal napelniony do pelna');
+        this.plyn = this.pojemnosc;
+        const info = document.getElementById('info');
+        document.getElementById("coffieCup").style.height = '100px';
+        wyczyscDiva('info');
+        info.insertAdjacentText('beforeend', 'Kubek zostal napelniony do pelna');
+        // console.log('Kubek zostal napelniony do pelna');
     }
 
+    wezlyk() {
+
+        const info = document.getElementById('info');
+        wyczyscDiva('info');
+
+        if (this.plyn === 0) {
+            info.insertAdjacentText('beforeend', 'Kubek jest juz pusty!');
+        } else {
+            this.plyn -= 20;
+            document.getElementById('coffieCup').style.height = this.plyn + 'px';
+            info.insertAdjacentText('beforeend', 'Wzieto lyk');
+        }
+    }
+
+    stanKubka() {
+
+        const info = document.getElementById('info');
+        wyczyscDiva('info');
+        if (this.plyn !== 0) {
+            info.insertAdjacentText('beforeend', `W kubku zostalo jeszcze ${this.plyn} jednostek.`);
+        } else {
+            info.insertAdjacentText('beforeend', 'Kubek jest juz pusty!');
+        }
+    }
+    
     umyj() {
         if (this.plyn === 0) {
             console.log('Kubek zostal umyty');
         } else {
             console.log('Kubel nie jest pusty, nie marnuj KAWY! DOPIJ!');
         }
-
     }
 
-    wezlyk() {
-        if (this.plyn===0) {
-            console.log('Kubek jest juz pusty!');
-        } else {
-            this.plyn -= 20;
-            document.getElementById('coffieCup').style.height=this.plyn+'px';
-            console.log('Wzieto lyk');
-        }
+    // kolorKubka() {
+    //     let wybranyKolor = document.getElementById('kolor');
+    //     switch (wybranyKolor){
+    //         case czarny:
+    //         document.getElementById('cup').style.backgroundColor = 'black';
+    //         break
+    //     }
 
-    }
+    // }
 
-    stanKubka(){
-        console.log('W kubku zostalo jeszcze ', this.plyn);
-    }
+}
 
+function wyczyscDiva(czyscik) {
+    document.getElementById(czyscik).innerHTML = "";
 }
 
 let kubekKawy = new Kubek(100);
